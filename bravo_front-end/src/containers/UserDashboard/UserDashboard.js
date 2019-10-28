b7d72fdcbb0d304cb2049e1da2ac4ac538fa4baf
pazma <pazma@oregonstate.edu>
2019-10-27 22:02:21 -0700

    Container and template for user dashboard.

---
+++ bravo_front-end/src/containers/UserDashboard/UserDashboard.js
@@ -0,0 +1,29 @@
+/* Package imports */
+import React from "react";
+import styled from "styled-components";
+
+/* Component Imports */
+/* Styling */
+const UserDashboardContainer = styled.div`
+  width: 100%;
+  height: calc(100vh - 75px);
+
+  background-color: lightgreen;
+`;
+
+/* COMPONENT */
+const UserDashboard = props => {
+  /* STATE VARIABLES ------------------------------------------ */
+  /* HOOKS ---------------------------------------------------- */
+  /* METHODS -------------------------------------------------- */
+  /* HANDLERS ------------------------------------------------- */
+  /* CONDITIONAL SUB-COMPONENTS ------------------------------- */
+  /* RENDER --------------------------------------------------- */
+  return (
+    <UserDashboardContainer>
+      Placeholder for the user dashboard
+    </UserDashboardContainer>
+  );
+};
+
+export default UserDashboard;
